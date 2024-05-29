4c0567001a7abb3f2c2986284b50bf0a3756fe44
shatVM <90845743+shatVM@users.noreply.github.com>
2024-05-29 10:55:49 +0300

    Додано посилання на фінальні проект учнів

--- final/final.js
+++ final/final.js
@@ -4,53 +4,398 @@
 //Визначення масиву об'єктів
 let itemsArray = [
     {
-        title: "Мотокоса 43",
-        image: "https://agora-discount.com/image/cache/catalog/10,05,2024/catalog-origin-308991-1144x912.jpg",
-        mono: 4,
-        pb: 10,
-        prise_standart: 5499,
-        prise_discount: 4497,
-        prise_coupon: 4395,
+        Group: "10-А 1",
+        LastName: "Боровий",
+        FirstName: "Артур",
+        Link: "https://23a-bam.github.io/GameMarket_Borovyi/market.html",
+        Mark: ""
     },
     {
-        title: "Електричний тример",
-        image: "",
-        mono: 6,
-        pb: 10,
-        prise_standart: 5000,
-        prise_discount: 4000,
-        prise_coupon: 3800,
+        Group: "10-А 1",
+        LastName: "Геча",
+        FirstName: "Кирило",
+        Link: "https://kyrylooo.github.io/Test1/",
+        Mark: ""
     },
     {
-        title: "Газонокосарка",
-        image: "",
-        mono: 6,
-        pb: 12,
-        prise_standart: 5000,
-        prise_discount: 4000,
-        prise_coupon: 3800,
+        Group: "10-А 1",
+        LastName: "Громова",
+        FirstName: "Марина",
+        Link: "",
+        Mark: ""
     },
     {
-        title: "Оприскувач",
-        image: "",
-        mono: 6,
-        pb: 8,
-        prise_standart: 5000,
-        prise_discount: 4000,
-        prise_coupon: 3800,
+        Group: "10-А 1",
+        LastName: "Гуменюк",
+        FirstName: "Аріна",
+        Link: "",
+        Mark: ""
     },
-
-
-   
-    
-
+    {
+        Group: "10-А 1",
+        LastName: "Демидчук",
+        FirstName: "Ілля",
+        Link: "https://zxcursed2008.github.io/GameMarket_Demydchuk/Final_GameMarket/market.html",
+        Mark: ""
+    },
+    {
+        Group: "10-А 1",
+        LastName: "Деркач",
+        FirstName: "Єлизавета",
+        Link: "https://liska228.github.io/GameMarket_Derkach-/final.market.html/market.html",
+        Mark: ""
+    },
+    {
+        Group: "10-А 1",
+        LastName: "Євланов",
+        FirstName: "Данііл",
+        Link: "https://scoxxy.github.io/GameMarket_YevlanovDaniil/FInal-gamemarket/market.html",
+        Mark: ""
+    },
+    {
+        Group: "10-А 1",
+        LastName: "Катя",
+        FirstName: "Булич",
+        Link: "https://katyablchh.github.io/GameMarket_Bulych/finalgamemarket/market.html",
+        Mark: ""
+    },
+    {
+        Group: "10-А 1",
+        LastName: "Ковальов",
+        FirstName: "Андрій",
+        Link: "https://andre00785.github.io/Game_Market_Kovalev/Final_hour/Gamemarket.html",
+        Mark: ""
+    },
+    {
+        Group: "10-А 1",
+        LastName: "Кожанов",
+        FirstName: "Юрій",
+        Link: "https://kyuk278937.github.io/StudentGame/",
+        Mark: ""
+    },
+    {
+        Group: "10-А 1",
+        LastName: "Козлюк",
+        FirstName: "Марина",
+        Link: "https://marykozl.github.io/GameMarket_Kozlyuk/final/market.html",
+        Mark: ""
+    },
+    {
+        Group: "10-А 1",
+        LastName: "Кравчук",
+        FirstName: "Іван",
+        Link: "https://maskenissen1.github.io/GameMarket_Kravchuk/Final-gamemarket/market.html",
+        Mark: ""
+    },
+    {
+        Group: "10-А 1",
+        LastName: "Лазаренко",
+        FirstName: "Софія",
+        Link: "https://sofialazarenko.github.io/GameMarket_Lazarenko/Final/final.html",
+        Mark: ""
+    },
+    {
+        Group: "10-А 1",
+        LastName: "Кайданюк",
+        FirstName: "Ілля",
+        Link: "https://nefenix.github.io/GameMarket_Kaidaniuk/Final-GameMarket/market.html",
+        Mark: ""
+    },
+    {
+        Group: "10-А 1",
+        LastName: "Марценюк",
+        FirstName: "Вадим",
+        Link: "https://jokerbrando.github.io/GameMarket_Martseniuk_Vadym/final%20market/market1.html",
+        Mark: ""
+    },
+    {
+        Group: "10-А 2",
+        LastName: "Будішевський",
+        FirstName: "Єгор",
+        Link: "",
+        Mark: ""
+    },
+    {
+        Group: "10-А 2",
+        LastName: "Мединський",
+        FirstName: "Роман",
+        Link: "https://medynskyiroman.github.io/market-test1/final/final.html",
+        Mark: ""
+    },
+    {
+        Group: "10-А 2",
+        LastName: "Михальський",
+        FirstName: "Нікіта",
+        Link: "",
+        Mark: ""
+    },
+    {
+        Group: "10-А 2",
+        LastName: "Недзвецький",
+        FirstName: "Олег",
+        Link: "",
+        Mark: ""
+    },
+    {
+        Group: "10-А 2",
+        LastName: "Ніцкевич",
+        FirstName: "Дар'я",
+        Link: "",
+        Mark: ""
+    },
+    {
+        Group: "10-А 2",
+        LastName: "Присяжнюк",
+        FirstName: "Богдана",
+        Link: "https://prydana.github.io/GameMarket_Prysiazhniuk/students/market_st.html",
+        Mark: ""
+    },
+    {
+        Group: "10-А 2",
+        LastName: "Сікалюк",
+        FirstName: "Артем",
+        Link: "",
+        Mark: ""
+    },
+    {
+        Group: "10-А 2",
+        LastName: "Соломонюк",
+        FirstName: "Богдан",
+        Link: "",
+        Mark: ""
+    },
+    {
+        Group: "10-А 2",
+        LastName: "Столяров",
+        FirstName: "Руслан",
+        Link: "",
+        Mark: ""
+    },
+    {
+        Group: "10-А 2",
+        LastName: "Столярчук",
+        FirstName: "Дмитро",
+        Link: "",
+        Mark: ""
+    },
+    {
+        Group: "10-А 2",
+        LastName: "Ткач",
+        FirstName: "Кирил",
+        Link: "",
+        Mark: ""
+    },
+    {
+        Group: "10-А 2",
+        LastName: "Філімончук",
+        FirstName: "Євгеній",
+        Link: "",
+        Mark: ""
+    },
+    {
+        Group: "10-А 2",
+        LastName: "Хмєль",
+        FirstName: "",
+        Link: "",
+        Mark: ""
+    },
+    {
+        Group: "10-А 2",
+        LastName: "Якубець",
+        FirstName: "Денис",
+        Link: "",
+        Mark: ""
+    },
+    {
+        Group: "10-Б 1",
+        LastName: "Башинська",
+        FirstName: "Вікторія",
+        Link: "https://viktoriabashinska.github.io/GameMarket_Bashynska/final/final.html",
+        Mark: ""
+    },
+    {
+        Group: "10-Б 1",
+        LastName: "Біденко",
+        FirstName: "Дмитро",
+        Link: "https://dimasvoinboecvolk.github.io/GameMarket-bidenkko/final/final.html",
+        Mark: ""
+    },
+    {
+        Group: "10-Б 1",
+        LastName: "Бовсуновського",
+        FirstName: "Максима",
+        Link: "https://maksbovs.github.io/GameMarket_Bovsunoskiy/final/final.html",
+        Mark: ""
+    },
+    {
+        Group: "10-Б 1",
+        LastName: "Волинець",
+        FirstName: "Владислав",
+        Link: "https://volynets-vlad.github.io/GameMarket_Volynets/final/final.html",
+        Mark: ""
+    },
+    {
+        Group: "10-Б 1",
+        LastName: "Галетко",
+        FirstName: "Артем",
+        Link: "https://artem4ikgaletko.github.io/GameMarket_Haletko/final/final.html?authuser=0",
+        Mark: ""
+    },
+    {
+        Group: "10-Б 1",
+        LastName: "Гордійчук",
+        FirstName: "Богдан",
+        Link: "https://malintovi.github.io/GameMarket_Hordiichuk/final/final.html?authuser=0",
+        Mark: ""
+    },
+    {
+        Group: "10-Б 1",
+        LastName: "Гоцман",
+        FirstName: "Артем",
+        Link: "https://golden-ztu.github.io/GameMarket_Hotsman/final/final.html",
+        Mark: ""
+    },
+    {
+        Group: "10-Б 1",
+        LastName: "Гуменюк",
+        FirstName: "Олег",
+        Link: "",
+        Mark: ""
+    },
+    {
+        Group: "10-Б 1",
+        LastName: "Дєдух",
+        FirstName: "Ілля",
+        Link: "https://dyeduh.github.io/GameMarket_Dyeduh/final/final.html?authuser=0",
+        Mark: ""
+    },
+    {
+        Group: "10-Б 1",
+        LastName: "Дідківській",
+        FirstName: "Андрій",
+        Link: "https://andriydidkivskij.github.io/GameMarket_Didkivskij/final/final.html",
+        Mark: ""
+    },
+    {
+        Group: "10-Б 1",
+        LastName: "Заєць",
+        FirstName: "Богдан",
+        Link: "",
+        Mark: ""
+    },
+    {
+        Group: "10-Б 1",
+        LastName: "Кравчук",
+        FirstName: "Андрій",
+        Link: "",
+        Mark: ""
+    },
+    {
+        Group: "10-Б 1",
+        LastName: "Кучер",
+        FirstName: "Максим",
+        Link: "https://kuchermaks.github.io/GameMarket_Kucher/final/final.html",
+        Mark: ""
+    },
+    {
+        Group: "10-Б 1",
+        LastName: "Лавренко",
+        FirstName: "Каріна",
+        Link: "https://karinalavrenko.github.io/GameMarket_Lavrenko/final/final.html?authuser=0",
+        Mark: ""
+    },
+    {
+        Group: "10-Б 2",
+        LastName: "Лещенко",
+        FirstName: "Настя",
+        Link: "https://leschenko1001.github.io/GameMarket_Leschenko/final/final.html?authuser=0",
+        Mark: ""
+    },
+    {
+        Group: "10-Б 2",
+        LastName: "Лобода",
+        FirstName: "Софія",
+        Link: "https://lobodass.github.io/siteinf/final/final.html?authuser=0",
+        Mark: ""
+    },
+    {
+        Group: "10-Б 2",
+        LastName: "Найбородін",
+        FirstName: "Артем",
+        Link: "https://nayborodin.github.io/GameMarket_Nayborodin/final/final.html",
+        Mark: ""
+    },
+    {
+        Group: "10-Б 2",
+        LastName: "Нікітіч",
+        FirstName: "Олександр",
+        Link: "",
+        Mark: ""
+    },
+    {
+        Group: "10-Б 2",
+        LastName: "Продоус",
+        FirstName: "Олександр",
+        Link: "https://oleksandrpr321.github.io/-GameMarket_Prodous/final/final.html",
+        Mark: ""
+    },
+    {
+        Group: "10-Б 2",
+        LastName: "Прокопчук",
+        FirstName: "Денис",
+        Link: "https://denis16543.github.io/GameMarket_Prokopchuk/final/final.html",
+        Mark: ""
+    },
+    {
+        Group: "10-Б 2",
+        LastName: "Савицкьий",
+        FirstName: "Єгор",
+        Link: "http://127.0.0.1:5500/final/final.html",
+        Mark: ""
+    },
+    {
+        Group: "10-Б 2",
+        LastName: "Саух",
+        FirstName: "Тимофій",
+        Link: "https://imnottimaw.github.io/GameMarket_Saukh/",
+        Mark: ""
+    },
+    {
+        Group: "10-Б 2",
+        LastName: "Скримінська",
+        FirstName: "Оксана",
+        Link: "http://127.0.0.1:5500/final/final.html",
+        Mark: ""
+    },
+    {
+        Group: "10-Б 2",
+        LastName: "Смолянова",
+        FirstName: "Ніка",
+        Link: "https://nikasms.github.io/GameMarket_Smolianova/final/final.html",
+        Mark: ""
+    },
+    {
+        Group: "10-Б 2",
+        LastName: "Терський",
+        FirstName: "Дмитро",
+        Link: "",
+        Mark: ""
+    },
+    {
+        Group: "10-Б 2",
+        LastName: "Шапіренко",
+        FirstName: "Максим",
+        Link: "https://shapirenko.github.io/GameMarket_Shapirenko/final/final.html?authuser=0",
+        Mark: ""
+    },
+    {
+        Group: "10-Б 2",
+        LastName: "Соколовська",
+        FirstName: "Вікторія",
+        Link: "https://vikasokol.github.io/market/market.html?authuser=0",
+        Mark: ""
+    }
 ]
-
-
-
-
-
-
 
 
 
@@ -63,40 +408,19 @@ let itemsDiv = document.getElementById("items");
 if (itemsDiv) {
     //Створення блоків по кількості елементів масиву
     itemsArray
+        .sort((a, b) => a.Group.localeCompare(b.Group))
         //Сортування за прізвищем
-        //.sort((a, b) => a.lastName.localeCompare(b.lastName))        
+        //.sort((a, b) => a.LastName.localeCompare(b.LastName))        
         //
         .forEach((item, index) => {
             //Виводимо на веб сторінку елемент масиву в блок з класом item
             itemsDiv.innerHTML +=
                 `
-                <!-- Блок окремого товару -->
-                <div class="item">
-    
-                    <div class="item-title">${item.title}</div>
-    
-                    <!-- Блок зображення товару -->
-                    <div class="item-image">
-                        <img src="${item.image}" class = "item-image">
-                    </div>
-    
-                    <!-- Блок з оплатою частинами -->
-                    <div class="parts-pay">
-                        <div><img src="img-market/mono-lapka.png" alt="">${item.mono}</div>
-                        <div><img src="img-market/pb.png" alt="">${item.pb}</div>
-                    </div>
-    
-                    <!-- Блок ціни -->
-                    <div class="prise">
-                        <div><span>${item.prise_standart} </span><sup>грн</sup></div>
-                        <div><span> ${item.prise_discount}</span><sup>грн</sup></div>
-                    </div>
-    
-                    <!-- Блок бонусної ціни -->
-                    <div class="prise bonus">
-                        <div>ціна за купоном</div>
-                        <div><span>${item.prise_coupon} </span><sup>грн</sup></div>
-                    </div>
+                <!-- Блок   -->
+                <div class="item">    
+                    <div class="item-title">${item.LastName} ${item.FirstName}  ${item.Group}</div>                    
+                    <a href = "${item.Link} " target="_blank" class = "bonus">Фінальний проект</a> 
+                    <div class="item-title">Оцінка: ${item.Mark}</div> 
                 </div>                
                 `
         })
